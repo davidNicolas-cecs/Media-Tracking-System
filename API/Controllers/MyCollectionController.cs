fab5b759442b039a57053e254ef161098d4d4fa9
davidNicolas-cecs <david.nicolasgarcia@student.csulb.edu>
2025-04-14 22:05:10 -0700

    details for book

---
+++ API/Controllers/MyCollectionController.cs
@@ -0,0 +1,26 @@
+﻿using MediaNest.Application.UseCases.UserCollectionManagment;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class MyCollectionController : Controller
+    {
+        private readonly IUserCollectionService _userCollectionService;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddToCollection([FromBody] int mediaItemId)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+    }
+}
